f13420b720ee8ff651fca4e1895eec85ae3cafd9
admin <rebbit123456@gmail.com>
2024-10-27 06:54:20 +0400

    init

--- src/components/Auth/SignUp.js
+++ src/components/Auth/SignUp.js
@@ -44,8 +44,9 @@ export default function SignUp() {
     };       
     await axios.post("http://127.0.0.1:5000/users_reg", form); 
     delete form.photo;
-    onClose()
     window.Telegram.WebApp.sendData(JSON.stringify(form)); 
+    onClose()
+    
   };
 
   const handleFileSelected = (event) => {
