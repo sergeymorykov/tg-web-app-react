65ea50a898ef80bce120273a5f0833041319aeb4
admin <rebbit123456@gmail.com>
2024-10-27 05:22:00 +0400

    init

--- src/components/Auth/SignUp.js
+++ src/components/Auth/SignUp.js
@@ -8,6 +8,173 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useTelegram } from "../../hooks/useTelegram";
+import Select from 'react-select';
+import makeAnimated from 'react-select/animated';
+import { useState, useCallback, useEffect } from 'react';
+import { useConstant } from '../Constant';
+import student_icon from './student-icon.png';
+import './SignUp.css';
+
+const animatedComponents = makeAnimated();
+const theme = createTheme();
+
+const SignUp = () => {
+    const [image, setImage] = useState(null);
+    const [selectedOption, setSelectedOption] = useState(null);
+    const { interests } = useConstant();
+    const { user_id, onClose, tg } = useTelegram();
+
+    const handleFileSelected = (event) => {
+        let file = event.target.files[0];
+
+        if (file) {
+            const reader = new FileReader();
+
+            // Event when file reading is finished
+            reader.onload = function (e) {
+                setImage(e.target.result);
+            };
+            reader.readAsDataURL(file);
+            let imageUrl = URL.createObjectURL(file);
+            document.getElementById('student_icon').src = imageUrl;
+        }
+    };
+
+    const handleChange = (selectedOption) => {
+        setSelectedOption(selectedOption);
+    };
+
+    const onSendData = useCallback(() => {
+        const form = {
+            id: user_id,
+            fullname: document.getElementById('lname').value + ' ' + document.getElementById('fname').value,
+            photo: image,
+            interests: selectedOption?.map((item) => item.value)
+        };
+
+        console.log(form);
+        window.Telegram.WebApp.sendData(JSON.stringify(form));
+        onClose();
+    }, [image, selectedOption, user_id, onClose]);
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData);
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData);
+        };
+    }, [onSendData, tg]);
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Регистрация'
+        });
+    }, [tg]);
+
+    useEffect(() => {
+        if (!image || !selectedOption) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }, [image, selectedOption, tg]);
+
+    return (
+      <ThemeProvider theme={theme}>
+          <Container component="main" maxWidth="xs">
+              <CssBaseline />
+              <Box
+                  sx={{
+                      marginTop: 8,
+                      display: 'flex',
+                      flexDirection: 'column',
+                      alignItems: 'center',
+                  }}
+              >
+                  <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }} src={student_icon} />
+                  <Typography component="h1" variant="h5">
+                      Регистрация
+                  </Typography>
+                  <Box component="form" noValidate sx={{ mt: 3 }}>
+                      <Grid container spacing={2}>
+                          <Grid item xs={12} sm={6}>
+                              <TextField
+                                  required
+                                  fullWidth
+                                  id="lname"
+                                  label="Фамилия"
+                                  name="lname"
+                                  autoComplete="family-name"
+                              />
+                          </Grid>
+                          <Grid item xs={12} sm={6}>
+                              <TextField
+                                  required
+                                  fullWidth
+                                  id="fname"
+                                  label="Имя"
+                                  name="fname"
+                                  autoComplete="given-name"
+                                  autoFocus
+                              />
+                          </Grid>
+                          <Grid item xs={12} textAlign="center">
+                              <label htmlFor="image-upload" className="upload-label">
+                                  <img src={student_icon} id='student_icon' width="200" height="200" alt="student icon" />
+                              </label>
+                              <input
+                                  type="file"
+                                  id="image-upload"
+                                  name="image"
+                                  accept="image/*"
+                                  onChange={handleFileSelected}
+                                  className="upload-input"
+                              />
+                          </Grid>
+                          <Grid item xs={12}>
+                              <Select
+                                  onChange={handleChange}
+                                  closeMenuOnSelect={false}
+                                  components={animatedComponents}
+                                  isMulti
+                                  options={interests}
+                                  className="basic-multi-select"
+                                  classNamePrefix="select"
+                              />
+                          </Grid>
+                          <Grid item xs={12}>
+                              <Button
+                                  type="button"
+                                  fullWidth
+                                  variant="contained"
+                                  onClick={onSendData}
+                                  sx={{ mt: 3, mb: 2 }}
+                              >
+                                  Регистрация
+                              </Button>
+                          </Grid>
+                      </Grid>
+                  </Box>
+              </Box>
+          </Container>
+      </ThemeProvider>
+  );
+};
+
+export default SignUp;
+
+
+/*
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import axios from 'axios';
 import student_icon from './student-icon.png';
 import "./SignUp.css";
@@ -146,3 +313,4 @@ export default function SignUp() {
     </ThemeProvider>
   );
 }
+  */
