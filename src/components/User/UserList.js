e3805b66679dc2cab6505e4a4cbee097ca5fc8b5
admin <rebbit123456@gmail.com>
2024-10-27 04:37:25 +0400

    init

--- src/components/User/UserList.js
+++ src/components/User/UserList.js
@@ -54,7 +54,7 @@ function UserList() {
                                         className='dislike'
                                         type="button"
                                         onClick={() => {
-                                            axios.post('http://127.0.0.1:5000/dislike', {critic_id: user_id, user_id: user.id, rating: 0});
+                                            axios.post('http://localhost:5000/dislike', {critic_id: user_id, user_id: user.id, rating: 0});
                                         }}
                                         variant="contained"
                                         sx={{ mt: 3, mb: 2 }}                                        
@@ -65,8 +65,9 @@ function UserList() {
                                     <Button
                                         className='like'
                                         type="button"
+                                        
                                         onClick={() => {
-                                            axios.post('http://127.0.0.1:5000/like', {critic_id: user_id, user_id: user.id, rating: 1});
+                                            axios.post('http://localhost:5000/like', {critic_id: user_id, user_id: user.id, rating: 1});
                                         }}
                                         variant="contained"  
                                         sx={{ mt: 3, mb: 2 }}                                      
